aec8ddabc99076128f8e0be4ee2396a850dd68c1
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-08-15 10:20:04 -0700

    finished formatting AWCA phyto data for PESP and wrote data file

--- programs/AWCA/AWCA_data_formatting.R
+++ programs/AWCA/AWCA_data_formatting.R
@@ -6,12 +6,9 @@
 #nicholas.rasmussen@water.ca.gov
 
 #script purpose
-#download data set published on EDI and
-#add some columns with metadata for this project
+#download AWCA phytoplankton data set published on EDI and
+#add some columns with metadata for the PESP project
 #so this dataset can be combined with other phyto data sets
-
-#to do list
-#probably need to convert time from PDT to PST
 
 #load packages---------
 library(tidyverse)
@@ -47,15 +44,23 @@ phyto_stn_na <- phyto_stn %>%
 phyto_format <- phyto_stn %>% 
   #drop some columns not needed by PESP
   select(-c(survey_year,survey_month)) %>% 
-  #change date time to PST
-  
-  #separate date and time columns
-  #mutate(date = ymd(date_time_pdt) 
-         #,time = hms(date_time_pdt)
-         #) %>% 
+  mutate(
+    #specify time zone as PDT for date-time
+    date_time_pdt = force_tz(date_time_pdt, tzone = "America/Los_Angeles")
+    #change date-time from PDT to PST
+    ,date_time_pst = with_tz(date_time_pdt,tzone="Etc/GMT+8")
+    #separate date and time columns
+    ,date = date(date_time_pst) 
+    ,time = format(as.POSIXct(date_time_pst),format = "%H:%M:%S")
+    #create taxon column with standardize name
+    ,taxon = case_when(taxon_current!="None"~taxon_current,TRUE~taxon_original)
+    ) %>% 
+  relocate(date_time_pst:time,.after = date_time_pdt) %>% 
+  relocate(taxon,.after = taxon_current) %>% 
   #add some columns with metadata about AWCA survey
   add_column(survey = "AWCA"
-             #,collection_type = #what is this?
+             ,collection_type = "grab_surface"
+             ,tidal_stage = "high_slack"
              ,depth = 0
              ,lab = "BSA"
              ) %>% 
@@ -63,25 +68,28 @@ phyto_format <- phyto_stn %>%
          ,station
          ,latitude
          ,longitude
-         #,collection_type
-         ,date_time_pdt
-         #,date
-         #,time
+         ,collection_type
+         ,date
+         ,time
+         ,tidal_stage
          ,depth
          ,lab
          ,taxon_original
-         ,taxon = taxon_current
+         ,taxon
          ,kingdom:algal_group
          ,genus
          ,species
          ,organisms_per_ml
          ,cells_per_ml
          ,biovolume_cubic_micron_per_ml
-         ,gald:debris
+         ,gald_micron = gald
+         ,quality_check
+         ,debris
          ) %>% 
   glimpse()
 
-
+#write formatted data file
+#write_csv(phyto_format, "./programs/AWCA/AWCA_phyto.csv")
 
 
 
